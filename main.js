147644449120ae37a44f68662b5bb98b026f5d13
Alexcmyk <alex.kirts@gmail.com>
2020-10-27 20:29:09 -0600

    start jquery functionality

---
+++ main.js
@@ -0,0 +1,10 @@
+$('.show-filters').on('click', function() {
+    
+    if($('.filters-list').is(':visible')) {   
+        $('.filters-list').slideUp()
+        $('.show-filters').text('Show filters')
+    } else {
+        $('.filters-list').slideDown()
+        $('.show-filters').text('Hide filters')
+    }
+})
